0944d493450c9d7e918c3c3dce0adc178e00c259
Francisco Lopez <45299869+lop7414@users.noreply.github.com>
2021-05-11 16:03:11 -0600

    cambios menores
    cambios siguiendo la guia para poder correr el Timer0 y UART de forma correcta

--- Lab7/LAB_7/main.c
+++ Lab7/LAB_7/main.c
@@ -1,3 +1,10 @@
+/***********************************************************************************************************
+ * Francisco Javier Lopez Menendez 1414
+ * Universidad del Valle de Guatemala
+ * Digital 2
+ */
+//**********************************************************************************************************
+
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/tm4c123gh6pm.h"
@@ -8,10 +15,12 @@
 #include "driverlib/interrupt.h"
 #include "driverlib/gpio.h"
 #include "driverlib/timer.h"
+
 #include "driverlib/uart.h"
 #include "driverlib/pin_map.h"
 
 uint32_t Status;
+uint32_t input;
 uint32_t LedR;
 uint32_t LedG;
 uint32_t LedB;
@@ -21,12 +30,10 @@ void UARTIntHandler(void){
 
     UARTIntClear(UART0_BASE, UARTIntStatus(UART0_BASE, true)); //input reset
 
-    uint32_t input;
-
-    input = UARTCharGet(UART0_BASE); //var de input
+    input = UARTCharGet(UART0_BASE); //Leer UART0
 
 
-    if(input == 'r' && LedR == 0){ //funciones para encender/apagar las LEDs
+    if(input == 'r' && LedR == 0){ //encender/apagar LEDR
         UARTCharPut(UART0_BASE, 'R');
         UARTCharPut(UART0_BASE, ' ');
         UARTCharPut(UART0_BASE, 'O');
@@ -47,7 +54,7 @@ void UARTIntHandler(void){
         LedR = 0;
     }
 
-    else if(input == 'g' && LedG == 0){
+    else if(input == 'g' && LedG == 0){//encender/apagar LEDG
         UARTCharPut(UART0_BASE, 'G');
         UARTCharPut(UART0_BASE, ' ');
         UARTCharPut(UART0_BASE, 'O');
@@ -68,7 +75,7 @@ void UARTIntHandler(void){
         LedG = 0;
     }
 
-    else if(input == 'b' && LedB == 0){
+    else if(input == 'b' && LedB == 0){//encender/apagar LEDB
         UARTCharPut(UART0_BASE, 'B');
         UARTCharPut(UART0_BASE, ' ');
         UARTCharPut(UART0_BASE, 'O');
@@ -95,38 +102,38 @@ void UARTIntHandler(void){
 
 int main(void){
     uint32_t ui32Period;
-//clock setup
+//Reloj
     SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
-//output setup
+//Perifericos
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
-//timer config
+//Configuracion Timer 0
     SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
     TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
-//uart setup
+//Configurar UART
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
-//period var get
+//Periodo de recepcion de variable
     ui32Period = SysCtlClockGet()/2;
     TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period -1);
-//ints enable
+//comenzar ints
     IntEnable(INT_TIMER0A);
     TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
 
-//timer enable
+//comenzar timer
     TimerEnable(TIMER0_BASE, TIMER_A);
 
 
     //GPIOPinConfigure(GPIO_PA0_U0RX);
     //GPIOPinConfigure(GPIO_PA1_U0TX);
-//uart pins
+//Pins UART
     GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-//uart config
+//Configuracion UART
     UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
 
-//UART ints
+//UART
     IntMasterEnable(); //master enable de ints
     IntEnable(INT_UART0);
     UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
